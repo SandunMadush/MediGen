18e5af9977295c958006cc4ebce67b6223e0f1d9
SandunMadush <sandunmadushanka57@gmail.com>
2022-11-13 01:06:53 +0530

    ward surgery width size

--- src/Ward.js
+++ src/Ward.js
@@ -22,26 +22,26 @@ const columns = [
   },
   { field: 'bht', headerName: 'BHT', type: 'string', width: 200, sortable: false },
   { field: 'date', headerName: 'Date', type: 'string', width: 100},
-  { field: 'surgery', headerName: 'Surgery', type: 'string', width: 200},
+  { field: 'surgery', headerName: 'Surgery', type: 'string', width: 75},
   { field: 'surgeon', headerName: 'Con Surgeon', type: 'string', width: 200}
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35, bht: '', date: '2022-05-12', surgery: '', surgeon: '' },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42, bht: '', surgery: '', surgeon: '' },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45, bht: '', surgery: '', surgeon: '' },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16, bht: '', surgery: '', surgeon: '' },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null, bht: '', surgery: '', surgeon: '' },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150, bht: '', surgery: '', surgeon: '' },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44, bht: '', surgery: '', surgeon: '' },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36, bht: '', surgery: '', surgeon: '' },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65, bht: '', surgery: '', surgeon: '' },
-  { id: 10, lastName: 'Roxie', firstName: 'Harvey', age: 65, bht: '', surgery: '', surgeon: '' }
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35, bht: 'MC/FD/12/15', date: '12/05/2022', surgery: 'IOL', surgeon: 'Dr. A Fernando' },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42, bht: 'GF/JH/16/48', date: '14/03/2022', surgery: 'PLI', surgeon: 'Dr. KDW Wijenayake' },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45, bht: 'HY/PO/14/47', date: '08/04/2022', surgery: 'KJT', surgeon: 'Dr. A Fernando' },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16, bht: 'RE/16/GH/14', date: '01/01/2022', surgery: 'IOL', surgeon: 'Dr. KDW Wijenayake' },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: 34, bht: 'GT/16/JU/14', date: '06/07/2022', surgery: 'GBR', surgeon: 'Dr. A Fernando' },
+  { id: 6, lastName: 'Melisandre', firstName: 'Princess', age: 10, bht: 'NU/LA/47/98', date: '11/06/2022', surgery: 'IOL', surgeon: 'Dr. A Fernando' },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44, bht: 'VC/JH/21/45', date: '09/03/2022', surgery: 'KJT', surgeon: 'Dr. A Fernando' },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36, bht: 'KI/MB/69/25', date: '13/02/2022', surgery: 'PLI', surgeon: 'Dr. KDW Wijenayake' },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65, bht: 'SA/DE/16/32', date: '11/02/2022', surgery: 'IOL', surgeon: 'Dr. A Fernando' },
+  { id: 10, lastName: 'Lestrange', firstName: 'Bellatrix', age: 56, bht: 'BN/KJ/16/74', date: '14/05/2022', surgery: 'PLI', surgeon: 'Dr. KDW Wijenayake' }
 ];
 
 export default function DataTable() {
   return (
-    <div style={{ height: 500, width: '100%' }}>
+    <div style={{ height: 500, width: '100%', display: "flex" }}>
       <DataGrid
         rows={rows}
         columns={columns}
